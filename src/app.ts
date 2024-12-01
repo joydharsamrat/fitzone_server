58267e5174e31f796643106c4fec1eaf9adae880
joydharsamrat <joydharsamrat@gmail.com>
2024-12-01 13:04:31 +0600

    bug fixed

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://fit-zone-three.vercel.app/",
+  "https://fit-zone-three.vercel.app",
 ];
 app.use(
   cors({
